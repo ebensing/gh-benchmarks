dab813a7986bac63442a3fb20604c8db5a7bd8f3
EJ Bensing <ebensing@rm-dash-r.com>
2013-07-09 17:35:26 -0700

    updated deps & added app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+
+// this will be the server that runs on your machine and does the actual
+// benchmarks
+
+var express = require('express');
+var mongoose = require('mongoose');
+var async = require('async');
+
+var app = express();
+
+
