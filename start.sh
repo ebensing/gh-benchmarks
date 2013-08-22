2d4fa1b016e00e104f1fe5355111155357d17bd7
EJ Bensing <ebensing@rm-dash-r.com>
2013-08-22 16:59:35 +0000

    tweaked startup script a little

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ mkdir -p ./logs/
 echo "Please enter your Github username:"
 read username
 echo "Please enter your Github password:"
-read password
+read -s password
 export githubUsername=$username 
 export githubPassword=$password 
 forever start -a -l $LOGPATH/logs/forever.log -o $LOGPATH/logs/out.log -e $LOGPATH/logs/err.log app.js
